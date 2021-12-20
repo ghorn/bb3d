951d4844809718c386be85f53a19714ca2a9d464
Greg Horn <greg@kittyhawk.aero>
2021-12-20 11:46:29 -0800

    exit -> std::exit in assert

--- bb3d/assert.cpp
+++ bb3d/assert.cpp
@@ -7,7 +7,7 @@ std::mutex g_exit_mutex;
 
 [[noreturn]] void exit_thread_safe(int exit_code) {
   const std::lock_guard<std::mutex> lock(g_exit_mutex);
-  exit(exit_code);  // NOLINT
+  std::exit(exit_code);  // NOLINT
 }
 
 };  // namespace bb3d
